2af94cf0653b7180ee04f0c0060f5c849749ad07
Viktor Polak <viktor.polak@tabula.live>
2023-10-23 10:01:44 +1300

    multi sfs

--- syncdist.sh
+++ syncdist.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 #rsync -vuLr --size-only --exclude current* --exclude latest* --exclude *.iso* rsync://mirror.aarnet.edu.au/pub/gentoo/releases/{amd64,arm64,riscv} releases
-rsync -vuL --size-only rsync://mirror.aarnet.edu.au/pub/gentoo/distfiles/* distfiles/
+for i in $(seq 0 15 | while read n; do printf "%01x " $n; done); do
+    rsync -vuL --size-only "rsync://mirror.aarnet.edu.au/pub/gentoo/distfiles/$i?/*" distfiles/
+done
